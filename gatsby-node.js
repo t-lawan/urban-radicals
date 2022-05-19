a41166ded9f7f6fd70d6ee8426ce2a561ab7f431
akinsola <thomas.lawanson@gmail.com>
2022-05-19 11:26:57 +0100

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -42,6 +42,7 @@ exports.createPages = async ({ graphql, actions }) => {
                 images {
                   gatsbyImageData
                 }
+                layout
                 text {
                   raw
                 }
