afe0118c0c62f62695a7d0049a5a0cbb0580d9d0
akinsola <thomas.lawanson@gmail.com>
2022-05-19 11:10:47 +0100

    Update selected-projects.styles.js

--- src/components/pages/selected-projects/selected-projects.styles.js
+++ src/components/pages/selected-projects/selected-projects.styles.js
@@ -7,12 +7,18 @@ export const SelectedProjectsWrapper = styled.div`/* padding: 1rem;
 export const SelectedProjectRow = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
+  padding: 1rem 0;
   :hover {
     > * > * {
+      /* opacity: .5; */
       color: ${props => props.colour} !important;
-      filter: sepia(100%) saturate(300%) brightness(70%) hue-rotate(180deg);
+      /* filter: sepia(100%) saturate(300%) brightness(70%) hue-rotate(180deg); */
+    }
 
-
+    > * {
+      opacity: .5;
+      /* color: ${props => props.colour} !important; */
+      /* filter: sepia(100%) saturate(300%) brightness(70%) hue-rotate(180deg); */
     }
   }
 `
