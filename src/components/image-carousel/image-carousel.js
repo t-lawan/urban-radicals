362048ccf3ce1d723d93a8fa18676b6c3b5e8d8a
akinsola <thomas.lawanson@gmail.com>
2022-05-24 07:03:47 +0100

    Create image-carousel.js

---
+++ src/components/image-carousel/image-carousel.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import { ImageCarouselWrapper } from "./image-carousel.styles";
+
+
+const ImageCarousel = () => {
+
+    return (
+        <ImageCarouselWrapper>
+            <p> Image Carousel</p>
+        </ImageCarouselWrapper>
+    )
+}
+
+export default ImageCarousel
