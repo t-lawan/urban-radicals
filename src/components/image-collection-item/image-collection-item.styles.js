e5f0606915ab00d80c60b93b41982e684a34f304
akinsola <thomas.lawanson@gmail.com>
2022-05-24 13:22:08 +0100

    Create image-collection-item.styles.js

---
+++ src/components/image-collection-item/image-collection-item.styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const ImageCollectionItemWrapper = styled.div`
+
+`
+
+export const ImageItemWrapper = styled.div`
+
+`
+
+export const Modal = styled.div`
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    background: red;
+    display: ${props => props.show ? 'block': 'none'};
+`
