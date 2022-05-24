995f0a5c487f89754b7fecea09dc9d8c6999d7b2
akinsola <thomas.lawanson@gmail.com>
2022-05-24 07:03:53 +0100

    Update ContentModel.js

--- src/models/ContentModel.js
+++ src/models/ContentModel.js
@@ -1,34 +1,38 @@
+import { Convert } from "../utils/convert"
+
 export class ContentBoxModel {
-    text;
-    images;
-    image_collection;
-    image_layout;
-    type;
+  text
+  images
+  image_collection
+  image_layout
+  type
 
-    constructor(text, images, image_collection, layout){
-
-        this.text = text;
-        this.images = images;
-        this.image_collection = image_collection;
-        this.image_layout = layout;
-        if(this.text){
-            this.type = ContentBoxType.TEXT_BLOCK
-        }
-
-        if(this.images){
-            this.type = ContentBoxType.IMAGE_BLOCK
-        }
-
-        if(this.image_collection){
-            this.type = ContentBoxType.IMAGE_COLLECTION_BLOCK
-        }
-
-
+  constructor(text, images, image_collection, layout) {
+    this.text = text
+    this.images = images
+    if (image_collection) {
+      this.image_collection = image_collection.map(image => {
+        return Convert.toContentModel(image)
+      })
     }
+
+    this.image_layout = layout
+    if (this.text) {
+      this.type = ContentBoxType.TEXT_BLOCK
+    }
+
+    if (this.images) {
+      this.type = ContentBoxType.IMAGE_BLOCK
+    }
+
+    if (this.image_collection) {
+      this.type = ContentBoxType.IMAGE_COLLECTION_BLOCK
+    }
+  }
 }
 
 export const ContentBoxType = {
-    TEXT_BLOCK: 'TEXT_BLOCK',
-    IMAGE_BLOCK: 'IMAGE_BLOCK',
-    IMAGE_COLLECTION_BLOCK: 'IMAGE_COLLECTION_BLOCK',
+  TEXT_BLOCK: "TEXT_BLOCK",
+  IMAGE_BLOCK: "IMAGE_BLOCK",
+  IMAGE_COLLECTION_BLOCK: "IMAGE_COLLECTION_BLOCK",
 }
