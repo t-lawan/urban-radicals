49c49c2c37f7bf29ae22d9e7024cb715001b44fa
akinsola <thomas.lawanson@gmail.com>
2022-05-24 13:21:42 +0100

    Create modal.styles.js

---
+++ src/components/modal/modal.styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const ModalWrapper = styled.div`
+    display: ${props => props.show ? 'block': 'none'};
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    z-index: 50;
+    background: red;
+    overflow-y: hidden;
+`
