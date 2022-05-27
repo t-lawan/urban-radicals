e0e341ac3d66f15e2b748e8bbac2689eaba10c38
akinsola <thomas.lawanson@gmail.com>
2022-05-27 10:07:31 +0100

    Update reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit"
-import { createStore } from "redux"
+import { ModalElementType } from "../utils/page-config"
 import * as ActionTypes from "./actions"
 const initalState = {
   showModal: false,
-  modalElement: null
+  modalElement: null,
+  modalElementType: null,
+  modalProject: null,
+  modalCarouselImages: [],
+  modalCarouselImageIndex: 0
 }
 
 const reducer = (state = initalState, action) => {
@@ -11,18 +15,36 @@ const reducer = (state = initalState, action) => {
     case ActionTypes.SHOW_MODAL:
       return Object.assign({}, state, {
         showModal: true,
-        modalElement: action.modalElement
+        modalElement: action.modalElement,
+        modalElementType: null
       })
+    case ActionTypes.SHOW_MODAL_PROJECT:
+      return Object.assign({}, state, {
+        showModal: true,
+        modalProject: action.modalProject,
+        modalElementType: ModalElementType.PROJECT_PAGE
+      })
+    case ActionTypes.SHOW_MODAL_CAROUSEL:
+        return Object.assign({}, state, {
+          showModal: true,
+          modalCarouselImages: action.modalCarouselImages,
+          modalCarouselImageIndex: action.modalCarouselImageIndex,
+          modalElementType: ModalElementType.IMAGE_CAROUSEL
+        })
     case ActionTypes.HIDE_MODAL:
       return Object.assign({}, state, {
         showModal: false,
-        modalElement: null
+        modalElement: null,
+        modalCarouselImages: [],
+        modalElementType: null
+
       })
 
     default:
       return state
   }
 }
+
 
 
 
