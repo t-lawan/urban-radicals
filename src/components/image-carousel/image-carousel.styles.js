96fd4f208d62af1bac3a3824b90c61b59b49e7d9
akinsola <thomas.lawanson@gmail.com>
2022-05-27 10:07:01 +0100

    Update image-carousel.styles.js

--- src/components/image-carousel/image-carousel.styles.js
+++ src/components/image-carousel/image-carousel.styles.js
@@ -1,5 +1,46 @@
 import styled from 'styled-components'
-
+import {
+    CarouselProvider,
+    Slider,
+    Slide,
+    ButtonBack,
+    ButtonNext,
+  } from "pure-react-carousel"
 export const ImageCarouselWrapper = styled.div`
 
+`
+
+export const StyledCarouselProvider = styled(CarouselProvider)`
+    width: 100vw;
+    height: 100vh;
+    background: blue;
+`
+
+export const StyledSlider = styled(Slider)`
+    width: 100%;
+    height: 85vh;
+    background: red;
+`   
+
+export const StyledSlide = styled(Slide)`
+
+`
+
+export const StyledButtonBack = styled(ButtonBack)`
+background: transparent;
+    border: 0;
+`
+
+export const StyledButtonNext= styled(ButtonNext)`
+background: transparent;
+    border: 0;
+`
+
+export const CarouselFooter = styled.div`
+    width: 100%;
+    bottom: 0;
+    position: absolute;
+    display: grid;
+    grid-template-columns: 1fr 5fr 1fr;
+    height: 15vh;
 `
