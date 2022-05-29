0afeabf965c24509fbb27513acb1728ea3ed1385
akinsola <thomas.lawanson@gmail.com>
2022-05-29 17:09:49 +0100

    Update image-carousel.js

--- src/components/image-carousel/image-carousel.js
+++ src/components/image-carousel/image-carousel.js
@@ -1,46 +1,80 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import "pure-react-carousel/dist/react-carousel.es.css"
-import {
-  CarouselProvider,
-  Slider,
-  Slide,
-  ButtonBack,
-  ButtonNext,
-} from "pure-react-carousel"
+import { getImage } from "gatsby-plugin-image"
+
 import {
   CarouselFooter,
+  Image,
+  ImageCaption,
   ImageCarouselWrapper,
+  ImageWrapper,
   StyledButtonBack,
   StyledButtonNext,
   StyledCarouselProvider,
   StyledSlide,
   StyledSlider,
 } from "./image-carousel.styles"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import { richTextOptions } from "../../utils/richtext"
 
 const ImageCarousel = props => {
-  console.log("imageCollection", props.imageCollection)
-  console.log("imageIndex", props.imageIndex)
+  const [imageIndex, SetImageIndex] = useState(0)
+  const [imageCollection, SetImageCollection] = useState([])
+
+  useEffect(() => {
+    // code to run on component mount
+    SetImageIndex(props.imageIndex)
+    SetImageCollection(props.imageCollection)
+  }, [])
+
+  const moveToPreviousImage = () => {
+    console.log("moveToPreviousImage")
+    console.log(imageCollection)
+
+    if (imageIndex > 0) {
+      SetImageIndex(imageIndex - 1)
+    }
+  }
+
+  const moveToNextImage = () => {
+    console.log(imageCollection)
+
+    console.log("moveToPreviousImage")
+    if (imageIndex < imageCollection.length - 1) {
+      SetImageIndex(imageIndex + 1)
+    }
+  }
+
   return (
     <ImageCarouselWrapper>
       <StyledCarouselProvider
         naturalSlideWidth={1000}
         naturalSlideHeight={1000}
-        totalSlides={props.imageCollection.length}
-        currentSlide={props.imageIndex}
+        totalSlides={imageCollection.length}
+        currentSlide={imageIndex}
       >
         <StyledSlider>
           {props.imageCollection.map((image, index) => (
             <StyledSlide key={index} index={index}>
-              I am the first Slide.
+              <ImageWrapper>
+                <Image image={getImage(image.images[0])} objectFit={'contain'} />
+              </ImageWrapper>
             </StyledSlide>
           ))}
         </StyledSlider>
         <CarouselFooter>
-          <StyledButtonBack>
-            <p>Back </p>
+          <StyledButtonBack onClick={() => moveToPreviousImage()}>
+            <p>Previous </p>
           </StyledButtonBack>
-          <p> Text </p>
-          <StyledButtonNext>
+          <ImageCaption>
+            {imageCollection.length > 0
+              ? documentToReactComponents(
+                  JSON.parse(imageCollection[imageIndex].text.raw),
+                  richTextOptions
+                )
+              : null}
+          </ImageCaption>
+          <StyledButtonNext onClick={() => moveToNextImage()}>
             <p>Next </p>
           </StyledButtonNext>
         </CarouselFooter>
