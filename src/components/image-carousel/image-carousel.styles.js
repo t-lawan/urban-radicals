9ba66d4c037fc4829ffdefbff864c8f8286c9ca3
akinsola <thomas.lawanson@gmail.com>
2022-05-29 17:09:52 +0100

    Update image-carousel.styles.js

--- src/components/image-carousel/image-carousel.styles.js
+++ src/components/image-carousel/image-carousel.styles.js
@@ -6,6 +6,23 @@ import {
     ButtonBack,
     ButtonNext,
   } from "pure-react-carousel"
+import { GatsbyImage } from "gatsby-plugin-image"
+
+
+export const Image = styled(GatsbyImage)`
+    width: 80%;
+    max-height: 80%;
+`
+
+export const ImageWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+
+`
 export const ImageCarouselWrapper = styled.div`
 
 `
@@ -13,13 +30,13 @@ export const ImageCarouselWrapper = styled.div`
 export const StyledCarouselProvider = styled(CarouselProvider)`
     width: 100vw;
     height: 100vh;
-    background: blue;
 `
 
 export const StyledSlider = styled(Slider)`
     width: 100%;
-    height: 85vh;
-    background: red;
+    height: 85%;
+    border: 3px solid black;
+
 `   
 
 export const StyledSlide = styled(Slide)`
@@ -43,4 +60,10 @@ export const CarouselFooter = styled.div`
     display: grid;
     grid-template-columns: 1fr 5fr 1fr;
     height: 15vh;
+`
+
+export const ImageCaption = styled.div`
+display: flex;
+    flex-direction: column;
+    justify-content: center;
 `
